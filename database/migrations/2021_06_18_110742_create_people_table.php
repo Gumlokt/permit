8edc37526985faecb0d6e39e91b53c857524d75f
Igor Kakupshev <ivkakupshev@yandex.ru>
2021-06-18 17:33:52 +0500

    feat: add migrations

---
+++ database/migrations/2021_06_18_110742_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('surname')->index();
+            $table->string('forename')->index();
+            $table->string('patronymic')->index();
+            $table->string('position')->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
---
+++ database/migrations/2021_06_18_110845_create_permits_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permits', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('people_id')->unsigned()->index();
+            $table->integer('companies_id')->unsigned()->index();
+
+            $table->string('number')->index();
+
+            $table->timestamp('start')->nullable();
+            $table->timestamp('end')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permits');
+    }
+}
